e8c98f5426ba00839b850684bfdc0c68f8c7819c
Joel S <joel29dec@gmail.com>
2019-03-28 15:29:56 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -2,7 +2,7 @@
 
 def loop_iterator(number_of_times)
     phrase = "Welcome to Flatiron School's Web Development Course!"
-    counter = 1
+    counter = 0
     loop do 
     puts phrase
     counter += 1
